6d21d092eca05c58d7c219a028aeaa6b5503c5c5
Bigends <chris.balkumar@outlook.fr>
2021-05-03 06:38:37 -0300

    JS EXO 2

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+// Déclaration variable pour déterminer un premier message
+var firstPart = "Premier message";
+alert(firstPart);
+
+// Déclaration 2e variable pour déterminer un deuxième message
+var lastPart = "Deuxième message";
+alert(lastPart);
